10d499862f76a5de5183688e02a9edc749ea234f
Marcos Penin <marcosgonzalezpenin@gmail.com>
2022-09-13 11:55:43 +0200

    add category controller

---
+++ src/main/java/guru/springfamework/domain/CategoryList.java
@@ -0,0 +1,15 @@
+package guru.springfamework.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class CategoryList {
+
+List<Category> categories;
+
+
+}
--- src/main/java/guru/springfamework/services/CategoryServiceImpl.java
+++ src/main/java/guru/springfamework/services/CategoryServiceImpl.java
@@ -8,7 +8,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepository categoryRepository;
